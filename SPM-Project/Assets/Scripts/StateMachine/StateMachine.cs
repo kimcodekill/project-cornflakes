8a30515d5b835f17ca14a3d38aa1c76a484898ea
shae <hackzmack@gmail.com>
2020-04-06 19:55:48 +0200

    Added functional STM structure and a dummy PlayerController component for testing.

---
+++ SPM-Project/Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateMachine {
+
+	private State currentState;
+
+	private Dictionary<Type, State> states = new Dictionary<Type, State>();
+
+	public StateMachine(object controller, State[] states) {
+		for (int i = 0; i < states.Length; i++) {
+			State instance = UnityEngine.Object.Instantiate(states[i]);
+			instance.owner = controller;
+			instance.stateMachine = this;
+
+			this.states.Add(instance.GetType(), instance);
+			currentState = currentState != null ? currentState : instance;
+		}
+		if (currentState != null) currentState.Enter();
+	}
+
+	public void Run() {
+		currentState.Run();
+	}
+
+	public void TransitionTo<T>() where T : State {
+		if (currentState != null) currentState.Exit();
+		currentState = states[typeof(T)];
+		currentState.Enter();
+	}
+
+}
