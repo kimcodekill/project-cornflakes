cf4385ea6e41b6d09f0256e93fc4a764ec7b70af
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-07 16:19:35 +0200

    Basic enemy behaviour AI: see player, shoot player. No bullet effect on player as of now.

---
+++ SPM-Project/Assets/Scripts/Enemy/EnemyBaseState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseState : State
+{
+    private SimpleEnemy enemy;
+
+    public SimpleEnemy Enemy => enemy = enemy != null ? enemy : (SimpleEnemy)Owner;
+}
