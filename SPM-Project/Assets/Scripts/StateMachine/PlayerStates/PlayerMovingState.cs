5ff27d9e62ebd79cafc8b5608f369fdc4d793275
shae <hackzmack@gmail.com>
2020-04-07 20:09:06 +0200

    Added customizability to PlayerState instances

--- SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerMovingState.cs
+++ SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerMovingState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PlayerState/MovingState")]
 public class PlayerMovingState : PlayerGroundedState {
 
+	public float Acceleration = 1f;
+
 	public override void Enter() {
 		if (StateMachine.ShowDebugInfo) Debug.Log("Entered PMT");
 	}
@@ -12,7 +14,7 @@ public class PlayerMovingState : PlayerGroundedState {
 	public override void Run() {
 		Vector3 input = Player.GetInput();
 		if (input.magnitude == 0) StateMachine.TransitionTo<PlayerStandingState>();
-		else Player.PhysicsBody.AddForce(input);
+		else Player.PhysicsBody.AddForce(input * Acceleration, ForceMode.Acceleration);
 		
 		base.Run();
 	}
