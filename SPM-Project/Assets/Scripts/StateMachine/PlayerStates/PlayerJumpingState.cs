1c84a54a01ed9837c19c1b992a1f6409c3fdcc0b
shae <hackzmack@gmail.com>
2020-04-07 20:09:06 +0200

    Added customizability to PlayerState instances

--- SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerJumpingState.cs
+++ SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerJumpingState.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PlayerState/JumpingState")]
 public class PlayerJumpingState : PlayerAirState {
 
+	public float JumpHeight = 6f;
+
 	public override void Enter() {
 		if (StateMachine.ShowDebugInfo) Debug.Log("Entered PJT");
-		Player.PhysicsBody.AddForce(Vector3.up * 6, ForceMode.Impulse);
+		Player.PhysicsBody.AddForce(Vector3.up * JumpHeight, ForceMode.Impulse);
 		
 		base.Enter();
 	}
