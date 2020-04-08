8281512b8260a64d5f9135aa71c745c2562ffabe
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-08 09:31:19 +0200

    added healthpack with pickup functionality for player, needs corresponding function for healing implemented in player class

---
+++ SPM-Project/Assets/Scripts/HealthPack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+    private BoxCollider bc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider>();        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            Debug.Log(other + " picked up healthpack. Healing " + other);
+            Destroy(gameObject);
+            //other.GetComponent<Player>().HealthRegen();
+        }
+           
+    }
+
+}
