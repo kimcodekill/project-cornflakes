0024cc90b8d2e567da60d2290534f4417508ac10
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-08 18:26:42 +0200

    tweaked bullet prefab. gave player reticle. tweaked EnemyTurret damage and rate of fire

--- SPM-Project/Assets/Scripts/Weapons/WeaponBase.cs
+++ SPM-Project/Assets/Scripts/Weapons/WeaponBase.cs
@@ -20,6 +20,7 @@ public abstract class WeaponBase : MonoBehaviour
     [SerializeField] [Tooltip("Bullet Max Distance")] protected float range;
     [SerializeField] [Tooltip("Bullet Travel Distance")] protected WeaponType weaponType;
     [SerializeField] [Tooltip("Layers bullets can hit")] protected LayerMask bulletMask;
+	[SerializeField] protected float damage;
 
     protected float shotDelay;
     protected float currentBulletsMagazine;
