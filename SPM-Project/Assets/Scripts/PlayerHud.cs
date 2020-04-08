563f2057105378dd1ba79fffe5fd16ef9d87da89
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-08 22:40:00 +0200

    added player hud

---
+++ SPM-Project/Assets/Scripts/PlayerHud.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHud : MonoBehaviour
+{
+	[SerializeField] Image panel;
+	[SerializeField] Text remainingBullets, magSize;
+	[SerializeField] Slider healthBar;
+	[SerializeField] PlayerController player;
+	// Start is called before the first frame update
+	private Color defaultPanelColour = new Color(1, 1, 1, 0);
+	private float flashTime;
+	private float flashDuration = 0.1f;
+    void Start()
+    {
+		panel.color = defaultPanelColour;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		healthBar.value = player.PlayerCurrentHealth;
+		//Debug.Log(player.PlayerCurrentHealth);
+		if ((Time.time - flashTime) > flashDuration)
+			ResetColor();
+    }
+
+	public void FlashColor(Color color) {
+		panel.color = color;
+		flashTime = Time.time;
+	}
+
+	private void ResetColor() {
+		panel.color = defaultPanelColour;
+	}
+}
