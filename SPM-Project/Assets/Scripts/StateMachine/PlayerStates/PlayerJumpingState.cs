fc5bbbbb56a7d232468d35f65b1921430507d94e
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-08 11:23:47 +0200

    fixed player jumpstate conflict

--- SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerJumpingState.cs
+++ SPM-Project/Assets/Scripts/StateMachine/PlayerStates/PlayerJumpingState.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "PlayerState/JumpingState")]
 public class PlayerJumpingState : PlayerAirState {
-	[SerializeField] private float jumpHeight;
+
+	public float JumpHeight = 6f;
+
 	public override void Enter() {
-		if (StateMachine.ShowDebugInfo) Debug.Log("Entered PJS");
-		Player.PhysicsBody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-		
+		if (StateMachine.ShowDebugInfo) Debug.Log("Entered PJT");
+		if (Input.GetKeyDown(KeyCode.Space) && Player.PhysicsBody.IsGrounded()) Player.PhysicsBody.AddForce(Vector3.up * JumpHeight, ForceMode.Impulse);
+
 		base.Enter();
 	}
 
