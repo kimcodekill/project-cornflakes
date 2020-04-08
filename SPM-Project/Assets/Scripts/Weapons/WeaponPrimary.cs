514fc25f256c0ba6e17ab0cadfadcfdfb03f8240
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-08 19:01:25 +0200

    reverted weapon changes. tweaked enemies, tweaked healthpack.

--- SPM-Project/Assets/Scripts/Weapons/WeaponPrimary.cs
+++ SPM-Project/Assets/Scripts/Weapons/WeaponPrimary.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class WeaponPrimary : WeaponBase
 {
+	private void Update() {
+		
+		Debug.DrawRay(transform.position, Camera.main.transform.forward * 20);
+		
+	}
+
     protected override void Shoot()
     {
         Vector3 spreadForward = AddSpread(Camera.main.transform.forward);
@@ -14,7 +20,7 @@ public class WeaponPrimary : WeaponBase
 			if (hit.collider.gameObject.CompareTag("Target")) hit.collider.gameObject.GetComponent<SimpleEnemy>().TakeDamage(damage);
         }
 
-        Camera.main.transform.forward = spreadForward + Camera.main.transform.forward;
+        //Camera.main.transform.forward = spreadForward + Camera.main.transform.forward;
 
         currentBulletsMagazine--;
     }
