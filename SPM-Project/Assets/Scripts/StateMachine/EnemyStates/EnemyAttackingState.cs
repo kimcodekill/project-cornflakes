41085cb623e39eb00456cdc0e69f1526d9e774c7
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-09 21:47:47 +0200

    Created enemy states for state machine. Altered SimpleEnemy into Enemy base class.

---
+++ SPM-Project/Assets/Scripts/StateMachine/EnemyStates/EnemyAttackingState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class EnemyAttackingState : EnemyBaseState
+{
+	[SerializeField] [Tooltip("Bullet object to instantiate when enemy attacks.")] private Bullet bulletPrefab;
+	[SerializeField] [Tooltip("How often/fast the enemy attacks.")] private float attackCooldown;
+	[SerializeField] [Tooltip("How much damage the enemy deals per shot.")] private float attackDamage;
+
+	private float internalAttackCD;
+
+
+	public override void Run() {
+		if(!Enemy.PlayerIsInSight()) { StateMachine.Pop(); }
+		internalAttackCD += Time.deltaTime;
+		if(internalAttackCD > attackCooldown) {
+			AttackTarget(Enemy.TargetVector);
+			internalAttackCD = 0;
+		}
+
+	}
+
+	private void AttackTarget(Vector3 v) {
+		Bullet instance;
+		instance = Instantiate(bulletPrefab, Enemy.GetGunPosition(), Enemy.transform.rotation);
+		instance.Initialize(v, v.magnitude);
+		Enemy.Target.TakeDamage(attackDamage);
+	}
+}
+
---
+++ SPM-Project/Assets/Scripts/StateMachine/EnemyStates/EnemyBaseState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    private Enemy enemy;
+    public Enemy Enemy => enemy = enemy != null ? enemy : (Enemy)Owner;
+
+	public override void Run() {
+		if(Enemy.PlayerIsInSight()) { StateMachine.Push<EnemyAlertedState>(); }
+	}
+
+}
