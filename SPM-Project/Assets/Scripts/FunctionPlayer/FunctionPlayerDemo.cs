e771daee419e07d7291f26518362c68945e2d1d1
shae <hackzmack@gmail.com>
2020-04-11 23:13:59 +0200

    Added delegate function controller

---
+++ SPM-Project/Assets/Scripts/FunctionPlayer/FunctionPlayerDemo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FpStd = FunctionPlayerStandardLibrary;
+
+public class FunctionPlayerDemo : MonoBehaviour {
+
+	private void Start() {
+		FunctionPlayer.EnqueueFunction(FpStd.SayHi);
+	}
+
+}
