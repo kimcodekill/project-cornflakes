08b453c9a7d0aa041a978a0045f98c06b5922dd0
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-17 11:34:00 +0200

    minor updates

--- SPM-Project/Assets/Scripts/StateMachine/EnemyStates/EnemyAlertedState.cs
+++ SPM-Project/Assets/Scripts/StateMachine/EnemyStates/EnemyAlertedState.cs
@@ -11,7 +11,9 @@ public class EnemyAlertedState : EnemyBaseState
 	}
 
 	public override void Run() {
+		
 		if(!Enemy.PlayerIsInSight()) { StateMachine.Pop(); }
+		
 		if (Enemy.TargetIsAttackable()) { StateMachine.Push<EnemyAttackingState>(); }
 		///Perhaps give some sort of time where the enemy stays alerted, doing things like searching for the player with an EnemySearchingState or similar
 	}
