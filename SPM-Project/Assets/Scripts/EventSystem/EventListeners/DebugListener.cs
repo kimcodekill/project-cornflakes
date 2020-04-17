0b8a91b6514910f44199882f0d2d65c4039909a4
shae <hackzmack@gmail.com>
2020-04-17 19:15:24 +0200

    Updated player prefab

--- SPM-Project/Assets/Scripts/EventSystem/EventListeners/DebugListener.cs
+++ SPM-Project/Assets/Scripts/EventSystem/EventListeners/DebugListener.cs
@@ -7,6 +7,8 @@ public class DebugListener : MonoBehaviour {
 	private void Start() => EventSystem.Current.RegisterListener<HitEvent>(OnEvent);
 
 	private void OnEvent(Event e) {
+		HitEvent he = e.GetReal();
+		Instantiate(Resources.Load("Debug/BulletHit"), he.HitPoint, Quaternion.identity);
 		Debug.Log(e.Description);
 	}
 
