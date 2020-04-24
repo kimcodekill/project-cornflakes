fd084858eaa7fac5e32818a99d553e697b59799a
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-25 00:59:43 +0200

    keeping old drone functionality for now, needs more work. Soldier should be unchanged with this commit.

--- SPM-Project/Assets/Scripts/Enemy/EnemyDrone.cs
+++ SPM-Project/Assets/Scripts/Enemy/EnemyDrone.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyDrone : Enemy {
 
 	[SerializeField] private float patrolBungeeDistance;
 	[SerializeField] private float movementSpeed;
 	[SerializeField] private SphereCollider body;
-	private NavMeshAgent agent;
-	private float bodyRadius = 0.5f;
-
+	
 	private Vector3 origin;
 
 	private void Awake() {
-		agent = GetComponent<NavMeshAgent>();
 		origin = transform.position;
 	}
 	private void Start() {
@@ -23,31 +19,28 @@ public class EnemyDrone : Enemy {
 
 	private void Update() {
 		base.Update();
-		
 	}
 
-	//private float DistanceToGround() {
-	//	Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, float.MaxValue, layerMask);
-	//	return hit.distance;
-	//}
-
-	//private Vector3 FindRandomPosition(Vector3 startingPos, float moveRange) {
-	//	Vector3 randomPos = startingPos + new Vector3(Random.Range(-moveRange, moveRange), Random.Range(-moveRange/3, moveRange/3), Random.Range(-moveRange, moveRange));
-	//	RaycastHit[] hits = Physics.SphereCastAll(transform.position, body.radius, (randomPos - transform.position).normalized, (randomPos - transform.position).magnitude + 1f, layerMask);
-	//	for (int i = 0; i < hits.Length; i++) {
-	//		if (hits[i].collider != null) {
-	//			//Debug.Log("Hit " + hits[i].collider.gameObject.name);
-	//			return FindRandomPosition(startingPos + hits[i].normal, moveRange);
-	//		}
-	//	}
-	//	return randomPos;
-	//}
+	private Vector3 FindRandomPosition(Vector3 startingPos, float moveRange) {
+		Vector3 randomPos = startingPos + new Vector3(Random.Range(-moveRange, moveRange), Random.Range(-moveRange/3, moveRange/3), Random.Range(-moveRange, moveRange));
+		RaycastHit[] hits = Physics.SphereCastAll(transform.position, body.radius, (randomPos - transform.position).normalized, (randomPos - transform.position).magnitude + 1f, layerMask);
+		for (int i = 0; i < hits.Length; i++) {
+			if (hits[i].collider != null) {
+				//Debug.Log("Hit " + hits[i].collider.gameObject.name);
+				return FindRandomPosition(startingPos + hits[i].normal, moveRange);
+			}
+		}
+		return randomPos;
+	}
 
 	private IEnumerator Patrol() {
-		agent.destination = FindNewRandomNavPoint(origin, patrolBungeeDistance);
-		while (agent.remainingDistance > 0.5f) {
+		Vector3 newPos = FindRandomPosition(origin, patrolBungeeDistance);
+		while (Vector3.Distance(transform.position, newPos) > 0.05f) {
+			transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, (newPos - transform.position), Time.deltaTime * 3f, 0f));
+			transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * movementSpeed);
 			yield return null;
 		}
+
 		yield return null;
 		StartCoroutine("Patrol");
 	}
@@ -70,32 +63,23 @@ public class EnemyDrone : Enemy {
 		StartCoroutine("Alerted");
 	}
 
-	private Vector3 FindNewRandomNavPoint(Vector3 startPos, float patrolRange) {
-		Vector3 newPoint = Random.insideUnitSphere * patrolRange;
-		newPoint = startPos + newPoint;
-		//Debug.Log(newPoint);
-		Vector3 finalPosition = startPos + Vector3.zero;
-		if (NavMesh.SamplePosition(newPoint, out NavMeshHit hit, patrolRange + agent.height, 1)) {
-			//Debug.Log(hit.position);
-			finalPosition = hit.position;
-		}
-		return finalPosition;
-	}
-
 	public override void StartPatrolBehaviour() {
+		//Debug.Log("Drone idling");
 		StartCoroutine("Patrol");
 	}
 
 	public override void StopPatrolBehaviour() {
-		
+		//Debug.Log("Drone stopping idling");
 		StopCoroutine("Patrol");
 	}
 
 	public override void StartAttackBehaviour() {
+		//Debug.Log("Drone attacking");
 		StartCoroutine("Attack");
 	}
 
 	public override void StopAttackBehaviour() {
+		//Debug.Log("Drone stopping attacking");
 		StopCoroutine("Attack");
 	}
 
