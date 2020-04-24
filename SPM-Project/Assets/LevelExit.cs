e792cc47861c507fcc1b258107ae047a48018f8a
Joakim <themanzed@gmail.com>
2020-04-24 15:37:21 +0200

    Set the pickup prefab position to (0,0,0); Expanded KimL area; Created LevelExit

---
+++ SPM-Project/Assets/LevelExit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] private bool visualizeVolume;
+    [SerializeField] private GameObject volume;
+
+    private BoxCollider collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+
+        VisualizeExit();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Player entered exit");
+            collider.enabled = false;
+        }
+    }
+
+    private void VisualizeExit()
+    {
+        volume.SetActive(visualizeVolume);
+        volume.transform.localScale = collider.size;
+    }
+}
