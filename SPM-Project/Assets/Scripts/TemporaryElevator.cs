8960d736a0969a6046c16830984ee5c461aa9c6a
LatentGecko <olszowy98@gmail.com>
2020-04-25 22:52:48 +0200

    added stuff, finished up my part of level2

---
+++ SPM-Project/Assets/Scripts/TemporaryElevator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryElevator : MonoBehaviour
+{
+    public Transform elevatorTarget;
+    public GameObject playerMech;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerMech.transform.position = elevatorTarget.transform.position;
+    }
+}
