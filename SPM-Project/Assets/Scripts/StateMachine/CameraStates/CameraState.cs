63f57b17e2a484e0d4d8ba8aa2e4642cb66d065a
Joakim <themanzed@gmail.com>
2020-04-26 15:54:48 +0200

    Created camerastates

---
+++ SPM-Project/Assets/Scripts/StateMachine/CameraStates/CameraState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraState : State
+{
+    private PlayerCamera camera;
+
+    public PlayerCamera Camera => camera = camera != null ? camera : (PlayerCamera)Owner;
+}
