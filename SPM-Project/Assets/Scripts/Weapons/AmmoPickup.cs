90fa4c7f85e93255c3cccf67d06c1fde4b9fa55f
shae <hackzmack@gmail.com>
2020-04-26 15:21:38 +0200

    Added particle effects to all pickups

--- SPM-Project/Assets/Scripts/Weapons/AmmoPickup.cs
+++ SPM-Project/Assets/Scripts/Weapons/AmmoPickup.cs
@@ -19,6 +19,19 @@ public class AmmoPickup : MonoBehaviour {
 
 	#endregion
 
+	private void Start() {
+		ParticleSystemRenderer psr = gameObject.AddComponent<ParticleSystemRenderer>();
+		ParticleSystem ps = gameObject.AddComponent<ParticleSystem>();
+		ps.startLifetime = 1f;
+		ps.startSize = 0.25f;
+		var ns = ps.shape;
+		ns.shapeType = ParticleSystemShapeType.Cone;
+		ns.rotation = new Vector3(-90f, 0f, 0f);
+		psr.material = new Material(gameObject.GetComponent<MeshRenderer>().material);
+		psr.material.shader = Shader.Find("Unlit/Color");
+		psr.material.SetColor("_Color", psr.material.GetColor("_Color") / 2f);
+	}
+
 	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Player")) {
 			EventSystem.Current.FireEvent(new PickUpEvent() {
