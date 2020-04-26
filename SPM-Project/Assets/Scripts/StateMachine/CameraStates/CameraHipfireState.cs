8c6c11ee4cb0349903c98b230f5653fffb7b2050
Joakim <themanzed@gmail.com>
2020-04-26 18:43:20 +0200

    here's the camera statemachine.

---
+++ SPM-Project/Assets/Scripts/StateMachine/CameraStates/CameraHipfireState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CameraState/HipfireState")]
+public class CameraHipfireState : CameraState
+{
+	//public int CameraFOV = 60;
+	//public Vector3 CameraOffset = new Vector3(1.75f, 1.5f, -7f);
+
+	public override void Run()
+	{
+		//No weapon? No hipfire.
+		if (!PlayerWeapon.Instance.WeaponIsActive) { StateMachine.Pop(); }
+		else if (Input.GetKeyDown(KeyCode.Mouse1) && PlayerWeapon.Instance.CurrentWeapon.AmmoType == Weapon.EAmmoType.Special) { StateMachine.Push<CameraScopedState>(); }
+		// ^: If the sniper is equipped and mouse1 is clicked, scope in.
+
+		base.Run();
+	}
+}
