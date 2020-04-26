34c85bd3ccede0eb5c77447e50f221958c2cb732
LatentGecko <olszowy98@gmail.com>
2020-04-26 20:01:43 +0200

    added soldiers to the map, added navmesh, fixed layers on some objects, modified decoration prefab

---
+++ SPM-Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void NextScene()
+    {
+        Debug.Log("something");
+        EventSystem.Current.FireEvent(new LevelEndEvent() {
+        Description = "PlayerEndedLevel",
+        NextLevel = 1 });
+    }
+}
