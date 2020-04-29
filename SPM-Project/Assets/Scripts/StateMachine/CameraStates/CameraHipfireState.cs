7ace352ebb6b6b4391338c2e75000a881d5e1430
shae <hackzmack@gmail.com>
2020-04-29 12:18:00 +0200

    Cleaned state scripts and added aim toggle toggle

--- SPM-Project/Assets/Scripts/StateMachine/CameraStates/CameraHipfireState.cs
+++ SPM-Project/Assets/Scripts/StateMachine/CameraStates/CameraHipfireState.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "CameraState/HipfireState")]
-public class CameraHipfireState : CameraState
-{
-	//public int CameraFOV = 60;
-	//public Vector3 CameraOffset = new Vector3(1.75f, 1.5f, -7f);
+public class CameraHipfireState : CameraState {
 
-	public override void Run()
-	{
-		//No weapon? No hipfire.
-		if (!PlayerWeapon.Instance.WeaponIsActive) { StateMachine.Pop(); }
-		if (Input.GetKeyDown(KeyCode.Mouse1) && PlayerWeapon.Instance.CurrentWeapon.AmmoType == Weapon.EAmmoType.Special) { StateMachine.Push<CameraScopedState>(); }
-		// ^: If the sniper is equipped and mouse1 is clicked, scope in.
+	public override void Run() {
+		if (PlayerWeapon.Instance.WeaponIsActive && Input.GetKeyDown(KeyCode.Mouse1) && StateMachine.CanEnterState<CameraScopedState>()) { StateMachine.TransitionTo<CameraScopedState>(); }
 
 		base.Run();
 	}
+
 }
