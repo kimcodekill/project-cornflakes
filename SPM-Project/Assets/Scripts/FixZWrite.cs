95a3801cc281a0b5e9fc6f4fb25638e2a4cc9af8
shae <hackzmack@gmail.com>
2020-04-29 15:24:28 +0200

    Added a tool for enabling zwriting on the specified mesh renderers

---
+++ SPM-Project/Assets/Scripts/FixZWrite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixZWrite : MonoBehaviour {
+	
+	/// <summary>
+	/// The mesh renderers whose materials to enable Z-writing on.
+	/// </summary>
+	public MeshRenderer[] MeshRenderers;
+	
+	private void Start() {
+		for (int i = 0; i < MeshRenderers.Length; i++) {
+			EnableZWrite(MeshRenderers[i]);	
+		}
+	}
+
+	private void EnableZWrite(MeshRenderer mr) {
+		Material m = new Material(mr.material);
+		mr.material = m;
+		m.SetInt("_ZWrite", 1);
+	}
+
+}
