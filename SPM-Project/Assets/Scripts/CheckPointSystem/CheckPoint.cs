a9fbef6ea16d8c7767d0a46b7d3ed524872fbcb9
shae <hackzmack@gmail.com>
2020-04-30 10:44:16 +0200

    Added chkptsys files

---
+++ SPM-Project/Assets/Scripts/CheckPointSystem/CheckPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	private bool triggered;
+
+	private void OnTriggerEnter(Collider other) {
+		if (!triggered) CaptureMoment();
+	}
+
+	private void CaptureMoment() {
+		
+	}
+
+}
