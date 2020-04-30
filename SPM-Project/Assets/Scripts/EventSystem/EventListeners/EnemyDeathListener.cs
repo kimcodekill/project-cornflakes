a8c790226f32bcce9994d578efcd5fcae5102cb3
ErikPilstrom <erik.pilstrom@gmail.com>
2020-04-30 13:37:19 +0200

    added EnemyDeathEvent and a listener for that event type for future use for scoring system and ammo drops

---
+++ SPM-Project/Assets/Scripts/EventSystem/EventListeners/EnemyDeathListener.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathListener : MonoBehaviour
+{
+	private void Start() => EventSystem.Current.RegisterListener<EnemyDeathEvent>(OnDeath);
+
+	private void OnDeath(Event e) {
+		EnemyDeathEvent ede = (EnemyDeathEvent) e;
+		Debug.Log(ede.Description);
+	}
+}
