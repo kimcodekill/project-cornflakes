d5e2f37b4023e3dff31fe769f7b5d0f9110b4d10
shae <hackzmack@gmail.com>
2020-05-02 14:39:25 +0200

    Added lootable interface

--- SPM-Project/Assets/Scripts/Enemy/EnemyTurret.cs
+++ SPM-Project/Assets/Scripts/Enemy/EnemyTurret.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyTurret : Enemy
-{
+public class EnemyTurret : Enemy, ILootable {
 
+	public LootTable GetLootTable() {
+		return new LootTable {
+			[typeof(HealthPack)] = 0.25f,
+			[typeof(AmmoPickup), Weapon.EAmmoType.Rockets] = 0.25f,
+			[typeof(AmmoPickup), Weapon.EAmmoType.Shells] = 0.25f,
+			[typeof(AmmoPickup), Weapon.EAmmoType.Special] = 0.25f,
+		};
+
+	}
 
 }
---
+++ SPM-Project/Assets/Scripts/ILootable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface ILootable {
+
+	/// <summary>
+	/// Here everything the implementor can drop and how it may drop it needs to be specified.
+	/// To create a loot table, simply declare a <c>new LootTable() { [lootType, (subType)] = dropChance, ... };</c>
+	/// </summary>
+	/// <returns>A loot table.</returns>
+	LootTable GetLootTable();
+
+}
+
+/// <summary>
+/// The loot table structure.
+/// </summary>
+public struct LootTable {
+
+	public struct LootTableItem {
+		public System.Type lootType;
+		public object subType;
+		public float dropChance;
+	}
+
+	private List<LootTableItem> lootTableItems;
+
+	/// <summary>
+	/// The indexer used to add loot table items to the loot table.
+	/// </summary>
+	/// <param name="lootType">What exactly should be dropped.</param>
+	/// <param name="subType">The desired subtype, if one exists.</param>
+	/// <returns>Nothing, actually.</returns>
+	public float this[System.Type lootType, object subType = null] {
+		set {
+			if (lootTableItems == null) lootTableItems = new List<LootTableItem>();
+			lootTableItems.Add(new LootTableItem() {
+				lootType = lootType,
+				subType = subType,
+				dropChance = value
+			});
+		}
+	}
+
+}
