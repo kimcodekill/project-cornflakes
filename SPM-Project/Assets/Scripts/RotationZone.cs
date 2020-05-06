2b32abb451ab2672f906f96fcfba1a9d72ecef03
shae <hackzmack@gmail.com>
2020-05-06 21:15:10 +0200

    Forgot to commit new rotation method oopsie

--- SPM-Project/Assets/Scripts/RotationZone.cs
+++ SPM-Project/Assets/Scripts/RotationZone.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class RotationZone : MonoBehaviour {
 
-	[SerializeField] private float rotationX;
-	[SerializeField] private float rotationY;
-
 	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Player")) {
-			other.gameObject.GetComponentInChildren<PlayerCamera>().InjectSetRotation(rotationX, rotationY);
+			other.gameObject.GetComponentInChildren<PlayerCamera>().InjectSetRotation(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y);
 			gameObject.SetActive(false);
 		}
 	}
