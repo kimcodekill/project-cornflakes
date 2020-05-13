d8f888b0e7caa5e18790dea6568cd0cceb50ef79
shae <hackzmack@gmail.com>
2020-05-13 16:45:49 +0200

    Added demo file

---
+++ SPM-Project/Assets/Scripts/Audio/AudioEventExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventExample : MonoBehaviour {
+
+	[SerializeField] private Audio someShitIWannaPlay;
+
+	private void OnTriggerEnter(Collider other) {
+		EventSystem.Current.FireEvent(new SFXEvent() {
+			Audio = someShitIWannaPlay
+		});
+	}
+
+}
