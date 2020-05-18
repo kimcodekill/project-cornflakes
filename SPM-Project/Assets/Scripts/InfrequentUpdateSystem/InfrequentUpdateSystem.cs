bb38502467cc695f1918c1e56c288da61bf07051
shae <hackzmack@gmail.com>
2020-05-18 11:50:26 +0200

    Made code slightly easier to read

--- SPM-Project/Assets/Scripts/InfrequentUpdateSystem/InfrequentUpdateSystem.cs
+++ SPM-Project/Assets/Scripts/InfrequentUpdateSystem/InfrequentUpdateSystem.cs
@@ -6,6 +6,19 @@ using UnityEngine;
 public class InfrequentUpdateSystem : MonoBehaviour {
 
 	/// <summary>
+	/// I made this to improve code readability in array accesses.
+	/// 0 is the function delegate,
+	/// 1 is the update interval,
+	/// 2 is the currently elapsed time since the last function call.
+	/// </summary>
+	private struct DataIndex {
+		public const int
+			Function = 0,
+			Interval = 1,
+			Elapsed = 2;
+	}
+
+	/// <summary>
 	/// The current static instance of the InfrequentSystem. Use this member to access the system.
 	/// </summary>
 	public static InfrequentUpdateSystem Current { get => current == null ? current = FindObjectOfType<InfrequentUpdateSystem>() : current; }
@@ -16,9 +29,6 @@ public class InfrequentUpdateSystem : MonoBehaviour {
 
 	/// <summary>
 	/// The data structure for the listener.
-	/// [0] is the function delegate,
-	/// [1] is the update interval,
-	/// [2] is the currently elapsed time since the last function call.
 	/// </summary>
 	private List<object[]> infrequentUpdateListeners;
 
@@ -40,7 +50,7 @@ public class InfrequentUpdateSystem : MonoBehaviour {
 		if (infrequentUpdateListeners == null || infrequentUpdateListeners.Count == 0) return;
 		int toRemoveIndex = 0;
 		for (int i = 0; i < infrequentUpdateListeners.Count; i++) {
-			if (infrequentUpdateListeners[i][0] == infrequentUpdate) toRemoveIndex = i;
+			if (infrequentUpdateListeners[i][DataIndex.Function] == infrequentUpdate) toRemoveIndex = i;
 		}
 		infrequentUpdateListeners.RemoveAt(toRemoveIndex);
 	}
@@ -48,15 +58,26 @@ public class InfrequentUpdateSystem : MonoBehaviour {
 	private void Update() {
 		if (infrequentUpdateListeners == null || infrequentUpdateListeners.Count == 0) return;
 		for (int i = 0; i < infrequentUpdateListeners.Count; i++) {
-			float interval = (float) infrequentUpdateListeners[i][1];
-			float elapsedTime = (float) infrequentUpdateListeners[i][2];
-			if ((elapsedTime += Time.deltaTime) > interval) {
+			float interval = (float) infrequentUpdateListeners[i][DataIndex.Interval];
+			float elapsedTime = (float) infrequentUpdateListeners[i][DataIndex.Elapsed];
+			if (IsTimeToRetrigger(elapsedTime += Time.deltaTime, interval)) {
 				elapsedTime = 0;
-				InfrequentUpdate listener = (InfrequentUpdate) infrequentUpdateListeners[i][0];
-				listener();
+				CallRegisteredFunction((InfrequentUpdate) infrequentUpdateListeners[i][DataIndex.Function]);
 			}
-			if (infrequentUpdateListeners.Count > 0) infrequentUpdateListeners[i][2] = elapsedTime;
+			if (infrequentUpdateListeners.Count > 0) infrequentUpdateListeners[i][DataIndex.Elapsed] = elapsedTime;
 		}
 	}
+
+	#region Helper Functions
+
+	private void CallRegisteredFunction(InfrequentUpdate infrequentUpdate) {
+		infrequentUpdate();
+	}
+
+	private bool IsTimeToRetrigger(float accumulatedTime, float interval) {
+		return accumulatedTime > interval;
+	}
+
+	#endregion
 
 }
