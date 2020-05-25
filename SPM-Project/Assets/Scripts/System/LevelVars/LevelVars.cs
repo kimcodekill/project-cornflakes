27341730d872000fbc8f30d60743317a09648540
Joakim <themanzed@gmail.com>
2020-05-25 16:56:39 +0200

    added LevelVar file

---
+++ SPM-Project/Assets/Scripts/System/LevelVars/LevelVars.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Level Variables")]
+public class LevelVars : ScriptableObject
+{
+    [SerializeField] private string levelPath = "Scenes/";
+    [SerializeField] private float parTime;
+
+    public string LevelPath { get => levelPath; }
+    public float ParTime { get => parTime; }
+}
