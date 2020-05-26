6ab8b31e272716e8a45e0e392ca5c65a0c59a737
Joakim <themanzed@gmail.com>
2020-05-26 12:49:38 +0200

    now there's a multiplier

--- SPM-Project/Assets/ScoreHandler.cs
+++ SPM-Project/Assets/ScoreHandler.cs
@@ -1,22 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+//Author: Joakim Linna
 public class ScoreHandler : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI multiplierText;
     [SerializeField] private float levelParTime = 120.0f;
-    [SerializeField] private string scoreKey = "score";
+    [SerializeField] private float multiplierTime;
+    
+    //private readonly string scoreKey = "score";
 
     private float score;
+    private int multiplier = 1;
+    private float multiplierStartTime;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     void Start()
     {
         //LoadScore();
+        
 
         EventSystem.Current.RegisterListener<EnemyDeathEvent>(OnEnemyDied);
         EventSystem.Current.RegisterListener<LevelEndEvent>(OnLevelEnd);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //temp
+        levelParTime = scene.buildIndex * 120.0f;
     }
 
     void FixedUpdate()
@@ -24,11 +41,26 @@ public class ScoreHandler : MonoBehaviour
         DisplayScore();
     }
 
+    private void Update()
+    {
+        if(multiplier != 1 && Time.time - multiplierStartTime > (multiplierTime / multiplier))
+        {
+            //ex: go from 3x to 1x
+            //multiplier = 1;
+
+            //ex: go from 3x to 2x
+            multiplier--;
+            multiplierStartTime = Time.time;
+        }
+    }
+
     private void OnEnemyDied(Event e)
     {
         EnemyDeathEvent ede = e as EnemyDeathEvent;
 
-        score += ede.ScoreValue;
+        score += ede.ScoreValue * multiplier;
+        multiplier++;
+        multiplierStartTime = Time.time;
     }
 
     private void OnLevelEnd(Event e)
@@ -44,5 +76,9 @@ public class ScoreHandler : MonoBehaviour
         //SaveScore();
     }
 
-    private void DisplayScore() { scoreText.text = "Score: " + score; }
+    private void DisplayScore() 
+    { 
+        scoreText.text = "Score: " + score;
+        multiplierText.text = multiplier + "X";
+    }
 }
