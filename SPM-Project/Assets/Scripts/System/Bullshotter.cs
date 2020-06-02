deb0bc13f260d45e114df07b54bb0b46ff3a02f1
shae <hackzmack@gmail.com>
2020-06-02 11:36:54 +0200

    Setup bullshot

---
+++ SPM-Project/Assets/Scripts/System/Bullshotter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullshotter : MonoBehaviour {
+
+	[SerializeField] private Material materialToAssign;
+	
+	void Start() {
+		GameObject[] all = FindObjectsOfType<GameObject>();
+		for (int i = 0; i < all.Length; i++) {
+			MeshRenderer mr = all[i].GetComponent<MeshRenderer>();
+			if (mr && mr.sharedMaterial.name.Equals("Default-Material")) {
+				mr.sharedMaterial = materialToAssign;
+			}
+		}
+	}
+
+}
