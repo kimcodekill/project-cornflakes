8d006f955edbdbd10cda53d5ffb215bb17473ef7
LatentGecko <olszowy98@gmail.com>
2020-06-03 21:30:49 +0200

    added space confirmation, changed the clip to be more epic maybe

--- SPM-Project/Assets/Scenes/Main/Intro/IntroSequence.cs
+++ SPM-Project/Assets/Scenes/Main/Intro/IntroSequence.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class IntroSequence : MonoBehaviour
 {
     private AudioSource audioSource;
+    private float timeDelay;
+    private bool skipAttempt;
     [SerializeField] private GameObject fadeImage;
     [SerializeField] private GameObject skipBox;
+    [SerializeField] private GameObject skipConfirmation;
     [SerializeField] private AudioClip introClip;
     [SerializeField] private Text dialogText;
     [SerializeField] private GameObject dialogBox;
@@ -17,35 +20,57 @@ public class IntroSequence : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = introClip;
-        dialogBox.active = false;
-        fadeImage.active = true;
-        skipBox.active = false;
+        skipAttempt = false;
+        dialogBox.SetActive(false);
+        fadeImage.SetActive(true);
+        skipBox.SetActive(false);
+        skipConfirmation.SetActive(false);
         StartCoroutine(IntroCoroutine());
+        timeDelay = 5;
     }
 
     void Update()
     {
+        timeDelay += Time.deltaTime;
         if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(SkipConfirmation());
+        }
+        if (timeDelay < 3 && Input.GetKeyDown("space"))
         {
             EndIntro();
         }
     }
 
+    private IEnumerator SkipConfirmation()
+    {
+        skipAttempt = true;
+        skipBox.SetActive(false);
+        skipConfirmation.SetActive(true);
+        yield return new WaitForSecondsRealtime(0.5f);
+        timeDelay = 0;
+        yield return new WaitForSecondsRealtime(2.5f);
+        skipConfirmation.SetActive(false);
+    }
+
     private IEnumerator IntroCoroutine()
     {
         audioSource.Play();
-        skipBox.active = true;
         yield return new WaitForSecondsRealtime(1f);
         fadeImage.GetComponent<Image>().CrossFadeAlpha(0, 5.0f, true);
         yield return new WaitForSecondsRealtime(2f);
         dialogText.GetComponent<Text>().text = "Five years";
-        dialogBox.active = true;
-        skipBox.active = false;
+        dialogBox.SetActive(true);
         yield return new WaitForSecondsRealtime(1.5f);
         dialogText.GetComponent<Text>().text = "Five long years of war";
+        if (timeDelay > 3 && skipAttempt == false)
+        {
+            skipBox.SetActive(true);
+        }
         yield return new WaitForSecondsRealtime(2.7f);
         dialogText.GetComponent<Text>().text = "And we're slowly losing";
         yield return new WaitForSecondsRealtime(2f);
+        skipBox.SetActive(false);
         dialogText.GetComponent<Text>().text = "The Scorthaza are ruthless";
         yield return new WaitForSecondsRealtime(2.2f);
         dialogText.GetComponent<Text>().text = "World after world have fallen to their slow but immovable march";
@@ -60,12 +85,12 @@ public class IntroSequence : MonoBehaviour
         yield return new WaitForSecondsRealtime(5f);
         dialogText.GetComponent<Text>().text = "but now they've reached the Inner Colonies";
         yield return new WaitForSecondsRealtime(2.3f);
-        //fadeImage.GetComponent<Image>().CrossFadeAlpha(1f, 5.0f, true);
         dialogText.GetComponent<Text>().text = "Arbesus is first on the line";
-        yield return new WaitForSecondsRealtime(3f);
-        fadeImage.GetComponent<Image>().CrossFadeAlpha(1f, 1.8f, true);
-        dialogBox.active = false;
-        yield return new WaitForSecondsRealtime(1.8f);
+        yield return new WaitForSecondsRealtime(2.8f);
+        fadeImage.GetComponent<Image>().CrossFadeAlpha(1f, 0.2f, true);
+        yield return new WaitForSecondsRealtime(0.2f);
+        dialogBox.SetActive(false);
+        yield return new WaitForSecondsRealtime(2f);
         EndIntro();
     }
 
