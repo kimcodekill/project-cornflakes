55218d5852949311e1bc0b60b789dcb9bce04da9
ErikPilstrom <erik.pilstrom@gmail.com>
2020-06-04 01:29:51 +0200

    drone loot change, might have been causing crash

--- SPM-Project/Assets/Scripts/Enemy/EnemyDrone.cs
+++ SPM-Project/Assets/Scripts/Enemy/EnemyDrone.cs
@@ -87,8 +87,8 @@ public class EnemyDrone : EnemyBase, ILootable {
 
 	public LootTable GetLootTable() {
 		return new LootTable {
-			["Pickups/Ammo/RocketsPickup"] = PlayerWeapon.Instance.HasWeaponOfAmmoType(Weapon.EAmmoType.Rockets) ? 0.1f : 0f,
-			["Pickups/Ammo/ShellsPickup"] = PlayerWeapon.Instance.HasWeaponOfAmmoType(Weapon.EAmmoType.Shells) ? 0.1f : 0f,
+			["Pickups/Ammo/RocketsPickup"] = PlayerWeapon.Instance.HasWeaponOfAmmoType(Weapon.EAmmoType.Rockets) ? 0.2f : 0f,
+			["Pickups/Ammo/ShellsPickup"] = PlayerWeapon.Instance.HasWeaponOfAmmoType(Weapon.EAmmoType.Shells) ? 0.2f : 0f,
 			["Pickups/Ammo/SpecialPickup"] = PlayerWeapon.Instance.HasWeaponOfAmmoType(Weapon.EAmmoType.Special) ? 0.2f : 0f,
 			[LootTable.Nothing] = 0.8f,
 		};
